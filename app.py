577cb00bee265522e9b2b2cad148e75729d7857a
kr <alxkipnis@gmail.com>
2022-12-04 23:55:28 +0500

    added proj files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import game_of_life
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    game_of_life.GameOfLife(25, 25)
+    return render_template("index.html")
+
+
+@app.route('/live')
+def live():
+    game_obj = game_of_life.GameOfLife()
+    game_obj.form_new_generation()
+
+    return render_template('live.html', game_obj=game_obj)
+
+
+if __name__ == '__main__':
+    app.run("127.0.0.1", 5000, debug=True)
